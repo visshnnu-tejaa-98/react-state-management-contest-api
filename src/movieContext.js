763bc7a82fda859f5c67c54a439d99ab37a9ff43
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-07 11:51:00 +0530

    created movieContext.js file

---
+++ src/movieContext.js
@@ -0,0 +1,24 @@
+import { useState, createContext } from 'react';
+
+export const MovieContext = createContext();
+
+export const MovieProvider = (props) => {
+	const [movies, setMovies] = useState([
+		{
+			id: 2342,
+			name: 'Harry Potter',
+			price: '$10',
+		},
+		{
+			id: 2343,
+			name: 'Game of Throns',
+			price: '$12',
+		},
+		{
+			id: 2344,
+			name: 'Bahubali',
+			price: '$13',
+		},
+	]);
+	return <MovieContext.Provider>{props.children}</MovieContext.Provider>;
+};
