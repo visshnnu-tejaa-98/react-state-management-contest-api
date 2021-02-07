a2e632169e7d5ab12e59f9f4d9fab5ca93af6bd6
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-07 12:31:29 +0530

    add movie functionality addedd

--- src/components/addMovie.js
+++ src/components/addMovie.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { MovieContext } from './movieContext';
 const AddMovie = () => {
 	let [name, setName] = useState('');
 	let [price, setPrice] = useState('');
+	let [movies, setMovies] = useContext(MovieContext);
 	const upDateName = (e) => {
 		setName(e.target.value);
 	};
 	const upDatePrice = (e) => {
 		setPrice(e.target.value);
 	};
+	const addMovie = (e) => {
+		e.preventDefault();
+		setMovies((prevMovies) => [...prevMovies, { name: name, price: price }]);
+	};
 	return (
-		<div>
+		<form onSubmit={addMovie}>
 			<input type='text' name='name' value={name} onChange={upDateName} />
 			<input type='text' name='price' value={price} onChange={upDatePrice} />
 			<button>Submit</button>
-		</div>
+		</form>
 	);
 };
 
